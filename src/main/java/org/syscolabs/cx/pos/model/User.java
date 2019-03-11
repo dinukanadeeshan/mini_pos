a52d3521e8d027f27dc20f0d05ff5bfdc920913c
dinuka kodithuwakku <dinuka.nadeeshan1993@gmail.com>
2019-03-11 13:58:42 +0530

    -integrate lombok

--- src/main/java/org/syscolabs/cx/pos/model/User.java
+++ src/main/java/org/syscolabs/cx/pos/model/User.java
@@ -1,41 +1,20 @@
 package org.syscolabs.cx.pos.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
     @Id
     private ObjectId _id;
     private String username;
     private String password;
-
-    public User(String username) {
-        this.username = username;
-    }
-
-
-    public ObjectId getId() {
-        return _id;
-    }
-
-    public void setId(ObjectId id) {
-        this._id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
